bc4985fd5814b9727f651b29487631de27fd07af
anushkaer2005 <anushkaer2005@gmail.com>
2026-02-03 21:37:57 +0530

    Day:15

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Day: 15
+
+console.log("loaded");
+
+// Variables
+let Name = "Anushka";                                        // string
+let Age = 20;                                                // number
+let present = true;                                          // boolean
+let Topics = ["HTML", "CSS"];                                // array
+let subject = {course: "Web Development",level: "two"};      // object               
+
+// Output
+console.log("Name:", Name);
+console.log("Age:", Age);
+console.log("Present:", present);
+console.log("Topics:", Topics);
+console.log("Subject:", subject);

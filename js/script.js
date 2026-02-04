d894beaf5fa8efeca04c5c2e4ee3e4d879dae7c6
anushkaer2005 <anushkaer2005@gmail.com>
2026-02-04 22:56:55 +0530

    Day:16

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
-// Day: 15
+// Day:15
 
-console.log("loaded");
+// console.log("loaded");
 
-// Variables
-let Name = "Anushka";                                        // string
-let Age = 20;                                                // number
-let present = true;                                          // boolean
-let Topics = ["HTML", "CSS"];                                // array
-let subject = {course: "Web Development",level: "two"};      // object               
+// // Variables
+// let Name = "Anushka";                                        // string
+// let Age = 20;                                                // number
+// let present = true;                                          // boolean
+// let Topics = ["HTML", "CSS"];                                // array
+// let subject = {course: "Web Development",level: "two"};      // object               
 
-// Output
-console.log("Name:", Name);
-console.log("Age:", Age);
-console.log("Present:", present);
-console.log("Topics:", Topics);
-console.log("Subject:", subject);
+// // Output
+// console.log("Name:", Name);
+// console.log("Age:", Age);
+// console.log("Present:", present);
+// console.log("Topics:", Topics);
+// console.log("Subject:", subject);
+
+// Day:16
+
+function greet(name) {
+  return "Hello, " + name;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function toFahrenheit(c) {
+  return (c * 9) / 5 + 32;
+}
+
+// Calling functions
+console.log(greet("Anushka"));
+console.log("Sum:", add(7, 8));
+console.log("Fahrenheit:", toFahrenheit(20));

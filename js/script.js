524303c275db7a4624e190a85f81efd4226bb204
anushkaer2005 <anushkaer2005@gmail.com>
2026-02-07 15:35:18 +0530

    Day 17 & 18

--- js/script.js
+++ js/script.js
@@ -16,21 +16,58 @@
 // console.log("Topics:", Topics);
 // console.log("Subject:", subject);
 
+
 // Day:16
 
-function greet(name) {
-  return "Hello, " + name;
-}
+// function greet(name) {
+//   return "Hello, " + name;
+// }
 
-function add(a, b) {
-  return a + b;
-}
+// function add(a, b) {
+//   return a + b;
+// }
 
-function toFahrenheit(c) {
-  return (c * 9) / 5 + 32;
-}
+// function toFahrenheit(c) {
+//   return (c * 9) / 5 + 32;
+// }
 
-// Calling functions
-console.log(greet("Anushka"));
-console.log("Sum:", add(7, 8));
-console.log("Fahrenheit:", toFahrenheit(20));
+// // Calling functions
+// console.log(greet("Anushka"));
+// console.log("Sum:", add(7, 8));
+// console.log("Fahrenheit:", toFahrenheit(20));
+
+
+// Day:17
+
+// Day:17 — Conditionals & Branching Logic
+
+// let ageEnter = prompt("Enter age:");
+
+// let age = Number(ageEnter);
+
+// if (ageEnter === "" || isNaN(age)) {
+//   alert("Invalid age");
+// } 
+// else {
+//   if (age < 18) {
+//     alert("Minor");
+//   } else if (age >= 18 && age <= 60) {
+//     alert("Adult");
+//   } else {
+//     alert("Senior");
+//   }
+// }
+
+
+// Day:18
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// for (let i = 0; i < 15; i++) {
+//   if (num[i] % 3 === 0) {
+//     console.log(num[i]);
+//   }
+// }
+
+
+// Day:19

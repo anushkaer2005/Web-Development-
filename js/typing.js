fa4766002df2abe0f8fa25ac622f05782688a11d
anushkaer2005 <anushkaer2005@gmail.com>
2026-02-11 21:16:27 +0530

    Day: 23

--- js/typing.js
+++ js/typing.js
@@ -7,3 +7,30 @@ if (typingInput && output) {
     output.textContent = typingInput.value;
   });
 }
+
+
+// Day 23
+const startBtn = document.getElementById("startBtn");
+const timeDisplay = document.getElementById("time");
+
+let time = 60;
+
+if (startBtn && typingInput && timeDisplay) {
+  startBtn.addEventListener("click", function () {
+
+    time = 60;
+    timeDisplay.textContent = time;
+    typingInput.disabled = false;
+
+    const timer = setInterval(function () {
+      time--;
+      timeDisplay.textContent = time;
+
+      if (time === 0) {
+        clearInterval(timer);
+        typingInput.disabled = true;
+        result.textContent ="Final Typed Length: " + typingInput.value.length;
+      }
+    }, 1000);
+  });
+}

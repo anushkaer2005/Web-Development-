60fb4e77798f070c878f0d8fd8db8ad90c914a6d
anushkaer2005 <anushkaer2005@gmail.com>
2026-02-12 14:02:09 +0530

    Day: 24

--- js/typing.js
+++ js/typing.js
@@ -14,6 +14,7 @@ const startBtn = document.getElementById("startBtn");
 const timeDisplay = document.getElementById("time");
 
 let time = 60;
+typingInput.disabled = true;
 
 if (startBtn && typingInput && timeDisplay) {
   startBtn.addEventListener("click", function () {
@@ -24,13 +25,42 @@ if (startBtn && typingInput && timeDisplay) {
 
     const timer = setInterval(function () {
       time--;
+      
+      if (time < 0) {
+        time = 0;
+      }
+      
       timeDisplay.textContent = time;
 
       if (time === 0) {
         clearInterval(timer);
         typingInput.disabled = true;
-        result.textContent ="Final Typed Length: " + typingInput.value.length;
+        
+        const typedLength = typingInput.value.length;
+        len.textContent = "Typed Length: " + typedLength;
+
+        // Save best score
+        if (typedLength > bestScore) {
+          bestScore = typedLength;
+          localStorage.setItem("bestScore", bestScore);
+          bestScoreText.textContent = bestScore;
+        }
       }
     }, 1000);
   });
 }
+
+
+// Day 24
+
+const bestScoreText = document.getElementById("bestScore");
+
+// Load best score when page loads
+let bestScore = localStorage.getItem("bestScore");
+
+if (bestScore === null) {
+  bestScore = 0;
+  localStorage.setItem("bestScore", bestScore);
+}
+
+bestScoreText.textContent = bestScore;
